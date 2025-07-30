b1137ea6da5694107c05f912368200e87e84667b
web-dev-pasta <on5100832@gmail.com>
2025-07-30 04:37:28 +0300

    Preloading All Images

--- src/App.js
+++ src/App.js
@@ -6,12 +6,24 @@ import Home from "./pages/Home/Home";
 import Technology from "./pages/Technology/Technology";
 import Destination from "./pages/Destination/Destination";
 import Header from "./components/Header/Header";
+
 function App() {
   const [items, setItems] = useState(null);
 
   useEffect(() => {
     axios.get(`${process.env.PUBLIC_URL}/data/data.json`).then(({ data }) => {
       setItems(data);
+
+      const allImageUrls = [
+        ...data.destinations.map((d) => d.images.png),
+        ...data.crew.map((c) => c.images.png),
+        ...data.technology.map((t) => t.images.portrait),
+      ];
+
+      allImageUrls.forEach((src) => {
+        const img = new Image();
+        img.src = src;
+      });
     });
   }, []);
   return (
